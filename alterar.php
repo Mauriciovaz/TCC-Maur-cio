8f6744567445162e5571ae82c6167dd0306ede25
Mauriciovaz <mauriciovaz698@gmail.com>
2020-07-29 02:15:03 -0300

    Atualização

---
+++ alterar.php
@@ -0,0 +1,43 @@
+<?php
+
+echo '<meta charset="UTF-8">';
+
+include "bd.php";
+session_start();
+
+$email = $_SESSION['email'];
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$foto1 = $_POST['foto1'];
+$sql = "SELECT * FROM cadastro WHERE email='$email'";
+   $resultado = mysqli_query($conexao,$sql);
+   if (mysqli_num_rows($resultado) > 0) {
+     while ($linha = mysqli_fetch_array($resultado)) {
+
+     	if($foto1 != $linha['foto']){
+
+     		$extensao = strtolower(substr($_FILES['foto']['name'], -4));
+			$novo_nome = md5(time()).$extensao;
+			$diretorio = "fotos/";
+
+  			move_uploaded_file($_FILES['foto']['tmp_name'], $diretorio.$novo_nome);
+
+     	}else{
+     		$novo_nome = $foto1;
+     	}
+       
+    }
+}
+
+    $sql = " UPDATE cadastro SET nome='$nome', senha='$senha', foto='$novo_nome' WHERE email='$email' ";
+
+$resultado = mysqli_query($conexao,$sql);
+
+if ($resultado) {
+	header("Location: index.php");
+}
+else{
+	echo "erro";
+}
+
+?>
--- pagina_aluno.php
+++ pagina_aluno.php
@@ -47,8 +47,54 @@ $resultFoto = $dados['foto'];
       <img  style="width: 100%;" src="fotos/<?php echo $resultFoto; ?>">
       <span class="white-text name" style="font-family: arial; font-weight: bold; font-size: 20px;"><blockquote><?php $nome = $_COOKIE['nome']; echo "$nome" ?> </blockquote></span>
       <span class="white-text email" style="font-size: 17px;"><?php echo "$email" ?></span>
+      <a href="#" data-target="slide-out1" class="sidenav-trigger show-on-large"> Configurações </a>
     </div></li>
-    <li><a class="waves-effect" href="sair.php" style="color: white; font-size: 17px; font-family: arial;">Sair</a></li>
+    <li>
+    <a class="waves-effect" href="sair.php" style="color: white; font-size: 17px; font-family: arial;">Sair</a>
+    </li>
+  </ul>
+
+  <ul id="slide-out1" class="sidenav" style="background-color: #37474f;">
+  <?php
+
+   $sql = "SELECT * FROM cadastro WHERE email='$email'";
+   $resultado = mysqli_query($conexao,$sql);
+   if (mysqli_num_rows($resultado) > 0) {
+     while ($linha = mysqli_fetch_array($resultado)) {
+       
+    ?>
+
+    <form action="alterar.php" method="POST" enctype="multipart/form-data">
+    <img  style="width: 100%;" src="fotos/<?php echo $resultFoto; ?>">
+    <div class="file-field input-field">
+        <div class="btn">
+          <span>Alterar foto</span>
+              <input type="file" name="foto" value="<?php echo $linha['foto'];?>">
+              </div>
+              <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" name="foto1" value="<?php echo $linha['foto'];?>">
+        </div>
+    </div>
+    
+    <p style="font-weight: bold;"> Nome: <input type="text" name="nome" value="<?php echo $linha['nome'];?>" style="color: white; width: 75%"></p>
+    <br>
+    <p style="font-weight: bold;"> Senha: <input type="password" name="senha" value="<?php echo $linha['senha'];?>" style="color: white; width: 75%"></p>
+    <br>
+    <br>
+    <div align="center">
+     <a class="btn #ff1744 red accent-3" href="pagina_aluno.php" style="color: white;">Voltar</a>
+     <button type="submit" class="btn #4caf50 green" style="color: white;">Salvar</button>
+   </div>
+      
+    
+   </form>
+   
+    <?php
+     }
+   }
+   
+
+   ?>
   </ul>
   
   
